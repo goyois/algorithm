e47d65f48d3c3fceaf895ee73499952721b323f3
Goyo <104404019+goyois@users.noreply.github.com>
2023-01-21 23:39:15 +0900

    [Bronze III] Title: 곱셈, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        String B = sc.next();
+
+        sc.close();
+
+        System.out.println(A * (B.charAt(2) - '0'));
+        System.out.println(A * (B.charAt(1) - '0'));
+        System.out.println(A * (B.charAt(0) - '0'));
+        System.out.println(A * Integer.parseInt(B));
+    }
+}
