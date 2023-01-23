51779a90eafe2f92e35b79d4fbcc9d55e9cf11f0
Goyo <104404019+goyois@users.noreply.github.com>
2023-01-24 03:02:34 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13924 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\\    /\\\n");
+        stringBuilder.append(" )  ( ')\n");
+        stringBuilder.append("(  /  )\n");
+        stringBuilder.append(" \\(__)|\n");
+
+        System.out.println(stringBuilder);
+    }
+}
