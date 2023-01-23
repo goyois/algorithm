00c8ab3a4a32f5c301b310ecff4b5aa5fe2bab98
Goyo <104404019+goyois@users.noreply.github.com>
2023-01-24 03:19:03 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("|\\_/|\n");
+        stringBuilder.append("|q p|   /}\n");
+        stringBuilder.append("( 0 )\"\"\"\\\n");
+        stringBuilder.append("|\"^\"`    |\n");
+        stringBuilder.append("||_/=\\\\__|\n");
+
+
+
+        System.out.println(stringBuilder);
+
+    }
+}
