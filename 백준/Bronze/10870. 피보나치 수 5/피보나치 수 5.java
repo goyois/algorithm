8e9dd8dfbb756cc81c163c28e6491079242aebf7
Goyo <104404019+goyois@users.noreply.github.com>
2023-02-08 22:23:50 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(fib(N));
+    }
+
+    static int fib(int N){
+        if (N == 0) return 0;
+        if (N == 1) return 1;
+        return fib(N-1) + fib(N-2);
+    }
+}
