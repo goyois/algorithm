54a1d9067d2e2679ea8889490779003e1d7261b0
Goyo <104404019+goyois@users.noreply.github.com>
2025-09-01 19:20:52 +0900

    [level 0] Title: 약수 구하기, Time: 0.29 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+		LinkedList<Integer> list = new LinkedList<>();
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				list.add(i);
+			}
+		}
+        
+		int[] arr = new int[list.size()];
+
+		for (int i = 0; i < list.size(); i++) {
+			arr[i] = list.get(i);
+		}
+        
+        return arr;
+    }
+}
