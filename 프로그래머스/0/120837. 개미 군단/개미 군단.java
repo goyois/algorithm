236ecf4df442a52c0e017b92de5f4f54e1906d0b
Goyo <104404019+goyois@users.noreply.github.com>
2025-09-01 17:06:37 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,19 +1,13 @@
 class Solution {
     public int solution(int hp) {
         int answer = 0;
-        int num1 = 0;
-		int num2 = 0;
         
-		answer = hp / 5;
-		num1 = hp % 5;
-
-		if (num1 != 0) {
-			answer += num1 / 3;
-			num2 = num1 % 3;
-		}
-		if (num2 != 0) {
-			answer += num2;
-		}
+		answer = hp / 5; 
+		hp = hp % 5;  
+		answer += hp / 3;
+		hp = hp % 3;
+		answer += hp;
+        
         return answer;
     }
 }
