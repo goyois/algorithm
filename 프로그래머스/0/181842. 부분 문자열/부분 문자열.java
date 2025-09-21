f91a689e1ef09695aec0bc5516fde2ef4afa7551
Goyo <104404019+goyois@users.noreply.github.com>
2025-09-21 19:50:56 +0900

    [level 0] Title: 부분 문자열, Time: 0.03 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        
+        answer = str2.indexOf(str1) == -1 ? 0 : 1;
+        
+        return answer;
+    }
+}
