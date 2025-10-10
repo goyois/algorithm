5c633c700df19a6178708677ef0e7373c441ecf6
Goyo <104404019+goyois@users.noreply.github.com>
2025-10-10 17:06:45 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] names) {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < names.length; i = i+5){
+            list.add(names[i]);
+        }
+        
+        String[] answer = new String[list.size()];
+        list.toArray(answer);
+        return answer;
+    }
+}
