23a2754604c7f2fc1c080d10183fb4d98b217d7d
Goyo <104404019+goyois@users.noreply.github.com>
2025-10-10 20:32:00 +0900

    [level 1] Title: 소수 만들기, Time: 59.70 ms, Memory: 93 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i+1; j < nums.length; j++) {
+				for (int k = j+1; k < nums.length; k++) {
+					int tmp = nums[i] + nums[j] + nums[k];
+					
+					if (tmp % 2 != 0) {
+						int cnt = 0;
+						for (int l = 2; l <= tmp; l++) {
+							if (tmp % l != 0) {
+								answer += 0;
+							} else {
+								cnt++;
+							}
+						}
+						
+						if (cnt == 1) {
+							answer++;
+						}
+					}
+				}
+			}
+		}
+        return answer;
+    }
+}
