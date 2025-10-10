04d31cf71e4b1bb26d1b0669176109afee4eded7
Goyo <104404019+goyois@users.noreply.github.com>
2025-10-10 16:28:06 +0900

    [level 0] Title: 할 일 목록, Time: 0.04 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+		ArrayList<String> list = new ArrayList<>();
+
+		for (int i = 0; i < finished.length; i++) {
+			if (finished[i] == false) {
+				list.add(todo_list[i]);
+			}
+		}
+		String[] answer = new String[list.size()];
+		list.toArray(answer);
+        return answer;
+    }
+}
