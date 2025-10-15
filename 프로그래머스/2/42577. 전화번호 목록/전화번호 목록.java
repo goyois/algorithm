2f7c819506431693bdd5fb5d826dba984536de11
Goyo <104404019+goyois@users.noreply.github.com>
2025-10-15 20:50:24 +0900

    [level 2] Title: 전화번호 목록, Time: 297.52 ms, Memory: 246 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+		HashSet<String> set = new HashSet<>(Arrays.asList(phone_book));
+
+		for (String s : phone_book) {
+			for (int i = 1; i < s.length(); i++) {
+				if (set.contains(s.substring(0, i))) {
+					return false;
+				}
+			}
+		}
+        return answer;
+    }
+}
