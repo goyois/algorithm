b5ad71e160f97e1e4d559467cd9d3ed8b4e8e07f
Goyo <104404019+goyois@users.noreply.github.com>
2025-10-15 19:19:20 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.97 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,31 +2,15 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
 		int[] answer = new int[score.length];
-		ArrayList<Integer> list = new ArrayList<>();
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        if (k >= score.length){
-            for (int i = 0; i < score.length; i++) {
-                list.add(score[i]);
-                answer[i] = Collections.min(list);
-            }
-            return answer;
-        } else {   
-		for (int i = 0; i < k; i++) {
-			list.add(score[i]);
-			answer[i] = Collections.min(list);
-		}
-		Collections.sort(list,Collections.reverseOrder());
-		for (int i = k; i < score.length; i++) {
-			if (score[i] >= list.get(k - 1)) {
-				list.add(score[i]);
-				Collections.sort(list, Collections.reverseOrder());
-				list.remove(list.size() - 1);
-				answer[i] = Collections.min(list);
-			} else {
-				answer[i] = Collections.min(list);
+		for (int i = 0; i < score.length; i++) {
+			pq.add(score[i]);
+			if (pq.size() > k) {
+				pq.remove();
 			}
+			answer[i] = pq.peek();
 		}
         return answer;
-        }
     }
 }
