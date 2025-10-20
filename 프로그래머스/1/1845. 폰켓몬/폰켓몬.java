624d198ce9d1c6102169e3931cad63012b2726eb
Goyo <104404019+goyois@users.noreply.github.com>
2025-10-20 20:23:42 +0900

    [level 1] Title: 폰켓몬, Time: 7.17 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,17 +1,18 @@
 import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public int solution(int[] nums) {
 		int n = nums.length / 2; //구해야하는 포켓몬 종류 수
 		int answer = 0;
-		HashSet<Integer> set = new HashSet<>();
-		for (int x : nums) {
-			set.add(x);
-		}
+		HashSet<Integer> set = Arrays.stream(nums)
+						.boxed()
+						.collect(Collectors.toCollection(HashSet::new));
+		
 		for (int i = 0; i < set.size(); i++) {
 			if (i < n) {
 				answer++;
 			}
-		}
-        return answer;
+		}        return answer;
     }
 }
