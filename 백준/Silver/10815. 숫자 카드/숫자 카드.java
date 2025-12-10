5d4d695d335616ac150038b1f71971cd26359f6f
Goyo <104404019+goyois@users.noreply.github.com>
2025-12-10 20:08:58 +0900

    Update 숫자 카드.java

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,26 +1,27 @@
+package algorithm.demo.baek.svr;
+
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class NumberCard {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         int[] arr1 = new int[n];
-        String line1 = br.readLine();
-        StringTokenizer st = new StringTokenizer(line1);
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++){
             arr1[i] = Integer.parseInt(st.nextToken());
         }
 
         int m = Integer.parseInt(br.readLine());
         int[] arr2 = new int[m];
-        String line2 = br.readLine();
-        st = new StringTokenizer(line2);
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
             arr2[i] = Integer.parseInt(st.nextToken());
         }
-
+        br.close();
 
         LinkedHashMap<Integer,Integer> map = new LinkedHashMap<>();
         for (int i = 0; i < m; i++) {
@@ -32,18 +33,12 @@ public class Main {
                 map.put(arr1[i],v+1);
             }
         }
-
-        int[] answer = new int[m];
-        int idx = 0;
-        for (Map.Entry<Integer,Integer> x : map.entrySet()){
-            answer[idx] = x.getValue();
-            idx++;
-        }
-
         StringBuilder sb = new StringBuilder();
-        for (int x : answer) {
-            sb.append(x +" ");
+        for (Map.Entry<Integer,Integer> x : map.entrySet()){
+            sb.append(x.getValue() +" ");
         }
-        System.out.println(sb.toString().stripTrailing());
+        bw.write(sb.toString().stripTrailing());
+        bw.flush();
+        bw.close();
     }
 }
