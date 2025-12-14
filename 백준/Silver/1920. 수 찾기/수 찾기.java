bba80f3ffaa2fddb62c7822b54eabb123c7cd895
Goyo <104404019+goyois@users.noreply.github.com>
2025-12-14 20:41:48 +0900

    [Silver IV] Title: 수 찾기, Time: 564 ms, Memory: 48792 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            if (set.contains(Integer.parseInt(st.nextToken()))) {
+                sb.append("1"+"\n");
+            } else sb.append("0"+"\n");
+        }
+        System.out.println(sb.toString().stripTrailing());
+    }
+}
