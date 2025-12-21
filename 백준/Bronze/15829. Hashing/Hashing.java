0d4baad9fd72891aceb056d23a14c4fdc29edab1
Goyo <104404019+goyois@users.noreply.github.com>
2025-12-21 15:28:54 +0900

    [Bronze II] Title: Hashing, Time: 176 ms, Memory: 17764 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -7,25 +7,16 @@ public class Main {
         sc.nextLine();
         String s = sc.nextLine();
 
-        char[] arr = new char[26];
-        char c = (char) 97;
-        for (int i = 0; i < 26; i++) {
-            arr[i] = c++;
-        }
+        long pow = 1;
+        int m = 1234567891;
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            int num = (int) c - 96;
+            sum += (num * pow) % m;
+            pow = 31 * pow % m;
 
-        int idx = 0;
-        double sum = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c1 = s.charAt(i);
-            for (int j = 0; j < arr.length; j++) {
-                if (c1 == arr[j]) {
-                    idx = j + 1;
-                    sum += idx * Math.pow(31,i);
-                    break;
-                }
-            }
         }
-        int answer = (int) sum;
-        System.out.println(answer);
+        System.out.println(sum%m);
     }
 }
