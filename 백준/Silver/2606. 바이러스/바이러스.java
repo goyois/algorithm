601a51d07b6c1a25d2dd4bc270340c0492e71ca8
Goyo <104404019+goyois@users.noreply.github.com>
2026-01-08 11:49:53 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -7,35 +7,34 @@ public class Main {
     static int n,m;
     static int[] visited;
     static ArrayList<ArrayList<Integer>> graph;
-    static int cnt = 0;
+    static int answer = 0;
 
 
     public void DFS(int v) {
         if (visited[v] == 0) visited[v] = 1;
         for (int i : graph.get(v)) {
             if (visited[i] == 0) {
+                answer++;
                 DFS(i);
-                cnt++;
             }
         }
     }
 
 
-
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
 
+        visited = new int[n+1];
         graph = new ArrayList<>();
-
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
-        visited = new int[n+1];
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
@@ -45,22 +44,10 @@ public class Main {
             graph.get(b).add(a);
         }
 
+        T.DFS(1);
 
-        for (int i = 1; i <= n; i++) {
-            if (!graph.get(i).isEmpty()) {
-                if (cnt > 0) break;
-                if (visited[i] == 0) {
-                    T.DFS(i);
-                }
-            }
-        }
-
-        int answer = 0;
-        for (int x : visited) {
-            answer += x;
-        }
-
-        if (answer == 0) System.out.println(0);
-        else System.out.println(answer-1);
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
     }
 }
