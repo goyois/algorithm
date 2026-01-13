82b483f830650f01402304556b5a62fb516462c1
Goyo <104404019+goyois@users.noreply.github.com>
2026-01-13 16:38:09 +0900

    [Silver II] Title: 최소 힙, Time: 280 ms, Memory: 26284 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        while (n-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            if (x > 0) {
+                pq.offer(x);
+            } else if (pq.isEmpty()) {
+                sb.append("0").append("\n");
+            } else {
+                sb.append(pq.poll()).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
